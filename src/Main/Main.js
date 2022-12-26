37d5e2f157633c437fe21d002e95b5af830da5ae
choibin12 <unmbb123@naver.com>
2022-12-26 14:18:07 +0900

    go!

--- src/Main/Main.js
+++ src/Main/Main.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import {Link} from "react-router-dom";
 
 const Main = () => {
     return (
         <div>
             Index
+
+            <button ><Link>관리자 ㄱ</Link></button>
         </div>
     );
 };
