0dc15d8cc1c6017a20129812916a70c4a44eb00b
choibin12 <unmbb123@naver.com>
2023-01-02 17:56:44 +0900

    ㅇㅇ

--- src/Admin/test.js
+++ src/Admin/test.js
@@ -2,39 +2,60 @@ import React, {useEffect, useState} from 'react';
 import axios from "axios";
 function Test(props) {
     const [data,setData] = useState([]);
-    const [moviecdNum,setMoviecdNum] = useState(20124079)
-    useEffect(()=>{
-        const url = 'http://www.kobis.or.kr/kobisopenapi/webservice/rest/movie/searchMovieInfo';
-        const key =  '?key=e158fbf9e424c0450708c4ea3e5bbdda';
-        const movieCd = `&movieCd=${moviecdNum}`;
+    const [moviecdNum,setMoviecdNum] = useState('');
+    /* useEffect(()=>{
+        const url = '"https://openapi.naver.com/v1/search/movie.json"';
 
-        axios.get( url+key+movieCd)
-            .then((res) =>setData(...data,res.data.movieInfoResult.movieInfo))
+        axios.get( url,{
+            params:{query: moviecdNum},
+            headers: {
+                "X-Naver-Client-Id": '_g6JfZzkITAmkjoExZi8',
+                "X-Naver-Client-Secret": 'SqBOobPA63',
+            },
+        })
+            .then((res) =>setData(...data,res.data))
 
-    },[moviecdNum]);
+    },[]); */
 
-    const mo =()=>{
-        setMoviecdNum(moviecdNum+1)
+    const mo = () =>{
+        const ID_KEY = 'eU4Ovj922C0R4HbxeRnV';
+        const SECRET_KEY = 'l4lR7UKZhF';
+        const search = moviecdNum;
+        try {
+            if (search === "") {
+                this.setState({movies: [], isLoading: false})
+            } else {
+                const {data: {
+                    items
+                }} = await axios.get('https://openapi.naver.com/v1/search/movie.json',{
+                    params:{
+                        query: search,
+                        display: 20
+                    },
+                    headers: {
+                        'X-Naver-Client-Id': ID_KEY,
+                        'X-Naver-Client-Secret': SECRET_KEY
+                    }
+                });
+
+                this.setData({movies: items, isLoading: false});
+            }
+        } catch (error) {
+            console.log(error);
+        }
+
     }
     return (
         <div>
             <table border={13}>
-                {
-                    console.log(data)
-                }
-                <thead>
                 <tr>
-                    <th>영화코드</th>
-                    <th>영화제목</th>
-                    <th>상영시간</th>
-                    <th>주연</th>
+                    <input id={moviecdNum} onChange={(e)=>setMoviecdNum(e.target.value)}/>
                 </tr>
-                </thead>
                 <tbody>
                             <tr>
                                 <td >{JSON.stringify(data)}</td>
                             </tr>
-                <button onClick={mo}>asd</button>
+                            <button onClick={mo} >1234</button>
                 </tbody>
             </table>
         </div>
