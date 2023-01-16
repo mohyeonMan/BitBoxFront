84554582f6ce1164d12ad9f5530ccefc7fd7bf0b
guk <lwk1202@naver.com>
2023-01-16 16:58:55 +0900

    관리자로그인문제수정

--- src/member/LoginForm2.js
+++ src/member/LoginForm2.js
@@ -11,7 +11,7 @@ import Container from '@mui/material/Container';
 import {createTheme, ThemeProvider} from '@mui/material/styles';
 import {useNavigate , Link} from "react-router-dom";
 import axios from "axios";
-import {setRefreshToken} from "src/member/storage/Cookie";
+import {removeCookieToken, setRefreshToken} from "src/member/storage/Cookie";
 
 const theme = createTheme();
 
@@ -58,12 +58,29 @@ const LoginForm2 = () => {
                                         password: aaa.password
                                     }).then(res => {
                                         if (res.data) {
-                                            alert(JSON.stringify(res.data));
-                                            setRefreshToken(res.data.refreshToken);
-                                            localStorage.setItem("accessToken", res.data.accessToken);
-                                            navi("/")
-                                            window.opener.parent.location.reload();
-                                            window.self.close();
+                                            setRefreshToken(res.data.refreshToken); // 쿠키에 리프레시토큰 저장
+                                            localStorage.setItem("accessToken", res.data.accessToken); // 로컬스토리지에 엑세스 토큰 저장
+                                            localStorage.setItem("expireTime", res.data.tokenExpiresIn); // 엑세스토큰 만료시간 저장
+
+                                            const accessTokenVal = localStorage.getItem("accessToken");
+
+                                            axios.get("/member/me", {
+                                                headers: {
+                                                    Authorization: `Bearer ${accessTokenVal}`
+                                                }
+                                            }).then(res => {
+                                                sessionStorage.setItem("userName", res.data.username);
+                                                sessionStorage.setItem("birth", res.data.birth);
+                                                console.log(res.data.name)
+                                                navi("/");
+                                                window.opener.parent.location.reload();
+                                                window.self.close();
+                                            }).catch(error => {
+                                                console.log("(토큰 만료시간(10분)되면 자동 로그아웃)에러 로그인하면 사라져요! " + error.response);
+                                                localStorage.removeItem('accessToken');
+                                                localStorage.removeItem('expireTime');
+                                                removeCookieToken();
+                                            })
                                         }
                                     }).catch(error => {
                                         console.log(error.response);
@@ -79,12 +96,29 @@ const LoginForm2 = () => {
                                                 password: aaa.password
                                             }).then(res => {
                                                 if (res.data) {
-                                                    alert(JSON.stringify(res.data));
-                                                    setRefreshToken(res.data.refreshToken);
-                                                    localStorage.setItem("accessToken", res.data.accessToken);
-                                                    navi("/")
-                                                    window.opener.parent.location.reload();
-                                                    window.self.close();
+                                                    setRefreshToken(res.data.refreshToken); // 쿠키에 리프레시토큰 저장
+                                                    localStorage.setItem("accessToken", res.data.accessToken); // 로컬스토리지에 엑세스 토큰 저장
+                                                    localStorage.setItem("expireTime", res.data.tokenExpiresIn); // 엑세스토큰 만료시간 저장
+
+                                                    const accessTokenVal = localStorage.getItem("accessToken");
+
+                                                    axios.get("/member/me", {
+                                                        headers: {
+                                                            Authorization: `Bearer ${accessTokenVal}`
+                                                        }
+                                                    }).then(res => {
+                                                        sessionStorage.setItem("userName", res.data.username);
+                                                        sessionStorage.setItem("birth", res.data.birth);
+                                                        console.log(res.data.name)
+                                                        navi("/");
+                                                        window.opener.parent.location.reload();
+                                                        window.self.close();
+                                                    }).catch(error => {
+                                                        console.log("(토큰 만료시간(10분)되면 자동 로그아웃)에러 로그인하면 사라져요! " + error.response);
+                                                        localStorage.removeItem('accessToken');
+                                                        localStorage.removeItem('expireTime');
+                                                        removeCookieToken();
+                                                    })
                                                 }
                                             }).catch(error => {
                                                 console.log(error.response);
@@ -139,10 +173,26 @@ const LoginForm2 = () => {
                 setRefreshToken(res.data.refreshToken); // 쿠키에 리프레시토큰 저장
                 localStorage.setItem("accessToken", res.data.accessToken); // 로컬스토리지에 엑세스 토큰 저장
                 localStorage.setItem("expireTime", res.data.tokenExpiresIn); // 엑세스토큰 만료시간 저장
-                navi("/")
-                window.opener.parent.location.reload();
-                window.self.close();
 
+                const accessTokenVal = localStorage.getItem("accessToken");
+
+                axios.get("/member/me", {
+                    headers: {
+                        Authorization: `Bearer ${accessTokenVal}`
+                    }
+                }).then(res => {
+                    sessionStorage.setItem("userName", res.data.username);
+                    sessionStorage.setItem("birth", res.data.birth);
+                    console.log(res.data.name)
+                    navi("/");
+                    window.opener.parent.location.reload();
+                    window.self.close();
+                }).catch(error => {
+                    console.log("(토큰 만료시간(10분)되면 자동 로그아웃)에러 로그인하면 사라져요! " + error.response);
+                    localStorage.removeItem('accessToken');
+                    localStorage.removeItem('expireTime');
+                    removeCookieToken();
+                })
             }
         }).catch(error => {
             console.log(error.response);
